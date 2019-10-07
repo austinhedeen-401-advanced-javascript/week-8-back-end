49bdb5a4835b30f8e692f1108b9affef09bc712c
Austin Hedeen <austin.hedeen@gmail.com>
2019-10-07 00:50:35 -0700

    Implement route to upload image to cloud storage and save URL to database

--- src/route/cloudinary.js
+++ src/route/cloudinary.js
@@ -5,8 +5,19 @@ const cloudinary = require('cloudinary');
 
 const router = express.Router();
 
-router.post('/cloudinary', (request, response) => {
-  const imageData = request.body.data;
+const Image = require('../model/image');
+
+router.post('/cloudinary', (request, response, next) => {
+  cloudinary.v2.uploader.upload(request.body.data,
+    {tags: 'test'},
+    (error, result) => {
+
+      // Save image to database
+      Image.create({ url: result.url })
+        .then(results => response.json(results))
+        .catch(next);
+    }
+  );
 });
 
 module.exports = router;
